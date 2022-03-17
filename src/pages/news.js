927d681a714bfffb09a450ae721ce15b27b3529e
Levente Karpati <1leventekarpati@gmail.com>
2022-03-17 08:52:07 +0000

    comments

--- src/pages/news.js
+++ src/pages/news.js
@@ -23,15 +23,17 @@ const rssContainer = {
 // }
 
 class News extends React.Component  {
+  //set default state
   constructor(props) {
     super(props);
     this.state = { 
       data: [] 
     };
   }
-
+  //api call using axios for rss feed
   componentDidMount() {
     var self = this;
+    //use rss2json with google rss api for warzone news data
     axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnews.google.com%2Frss%2Fsearch%3Fq%3Dcall%2Bof%2Bduty%2Bwarzone%26hl%3Den-GB%26gl%3DGB%26ceid%3DGB%3Aen&api_key=vlpqsg5fjf9ogebcmrpoz8hdchihae9nywlrezcy&order_by=pubDate&order_dir=desc&count=100')
     .then(function (res) {
       self.setState({data: res.data.items});
@@ -41,13 +43,14 @@ class News extends React.Component  {
       console.log(err);
     });
   }
-  
+  //render html
   render() {
     return (
       <Main>
         <div style={newsMainStyles}>
           <div style={rssContainer}>
             {
+              //map state date to html
               this.state.data.map(function(item, i){
                 return <p key={i}>{item.title}</p>
               })
